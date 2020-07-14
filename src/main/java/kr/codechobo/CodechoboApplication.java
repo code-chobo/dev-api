03d29397d9e280d517415741d4dc2cc78898e4a2
Grace <govlmo91@gmail.com>
2020-07-14 11:41:15 +0900

    init

---
+++ src/main/java/kr/codechobo/CodechoboApplication.java
@@ -0,0 +1,13 @@
+package kr.codechobo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodechoboApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodechoboApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kr/codechobo/CodechoboApplicationTests.java
@@ -0,0 +1,13 @@
+package kr.codechobo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodechoboApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
